65138cfc62d3e33d39ff08cbcd4ac18de46349bc
Rodrigo Nunes de Castro <rodrigolnr@gmail.com>
2013-01-23 15:08:23 -0200

    menu dispensado

--- templates/geral/menu.php
+++
@@ -1,8 +0,0 @@
-<a href='/<?php echo BASE; ?>/index.php/clientes/cadastrar/'>Cadastrar Cliente</a> |
-<a href='/<?php echo BASE; ?>/index.php/clientes/listar/'>Listar Clientes</a> 
-<br />
-<a href='/<?php echo BASE; ?>/index.php/servico/cadastrar/'>Cadastrar Serviços</a> | 
-<a href='/<?php echo BASE; ?>/index.php/servico/listar/'>Listar Serviços</a> 
-<br />
-<a href='/<?php echo BASE; ?>/index.php/estoque/cadastrar/'>Cadastrar Estoque</a> | 
-<a href='/<?php echo BASE; ?>/index.php/estoque/listar/'>Listar Estoque</a> 
